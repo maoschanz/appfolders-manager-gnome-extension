3b06d1cf02fd2b14eb1d9a0e8b64c93ea81095d9
Maestroschan <rrroschan@gmail.com>
2017-05-29 21:43:03 +0200

    Update convenience.js

--- convenience.js
+++ convenience.js
@@ -1,310 +1,93 @@
-const Clutter = imports.gi.Clutter;
+/* -*- mode: js; js-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+  Copyright (c) 2011-2012, Giovanni Campagna <scampa.giovanni@gmail.com>
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the GNOME nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+const Gettext = imports.gettext;
 const Gio = imports.gi.Gio;
-const Lang = imports.lang;
-const St = imports.gi.St;
-const Main = imports.ui.main;
-const AppDisplay = imports.ui.appDisplay;
-const PopupMenu = imports.ui.popupMenu;
-const Mainloop = imports.mainloop;
 
-const ModalDialog = imports.ui.modalDialog;
-const ShellEntry = imports.ui.shellEntry;
-
+const Config = imports.misc.config;
 const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-const Convenience = Me.imports.convenience;
 
-const Gettext = imports.gettext.domain('appfolders-manager');
-const _ = Gettext.gettext;
+/**
+ * initTranslations:
+ * @domain: (optional): the gettext domain to use
+ *
+ * Initialize Gettext to load translations from extensionsdir/locale.
+ * If @domain is not provided, it will be taken from metadata['gettext-domain']
+ */
+function initTranslations(domain) {
+    let extension = ExtensionUtils.getCurrentExtension();
 
-//-------------------------------------------------
+    domain = domain || extension.metadata['gettext-domain'];
 
-let _foldersSchema;
-let _folderList;
-
-function init() {
-    Convenience.initTranslations();
+    // check if this extension was built with "make zip-file", and thus
+    // has the locale files in a subfolder
+    // otherwise assume that extension has been installed in the
+    // same prefix as gnome-shell
+    let localeDir = extension.dir.get_child('locale');
+    if (localeDir.query_exists(null))
+        Gettext.bindtextdomain(domain, localeDir.get_path());
+    else
+        Gettext.bindtextdomain(domain, Config.LOCALEDIR);
 }
 
-//-------------------------------------------------
+/**
+ * getSettings:
+ * @schema: (optional): the GSettings schema id
+ *
+ * Builds and return a GSettings schema for @schema, using schema files
+ * in extensionsdir/schemas. If @schema is not provided, it is taken from
+ * metadata['settings-schema'].
+ */
+function getSettings(schema) {
+    let extension = ExtensionUtils.getCurrentExtension();
 
-function injectToFunction(parent, name, func) {
-	let origin = parent[name];
-	parent[name] = function() {
-		let ret;
-		ret = origin.apply(this, arguments);
-			if (ret === undefined)
-				ret = func.apply(this, arguments);
-			return ret;
-		}
-	return origin;
+    schema = schema || extension.metadata['settings-schema'];
+
+    const GioSSS = Gio.SettingsSchemaSource;
+
+    // check if this extension was built with "make zip-file", and thus
+    // has the schema files in a subfolder
+    // otherwise assume that extension has been installed in the
+    // same prefix as gnome-shell (and therefore schemas are available
+    // in the standard folders)
+    let schemaDir = extension.dir.get_child('schemas');
+    let schemaSource;
+    if (schemaDir.query_exists(null))
+        schemaSource = GioSSS.new_from_directory(schemaDir.get_path(),
+                                                 GioSSS.get_default(),
+                                                 false);
+    else
+        schemaSource = GioSSS.get_default();
+
+    let schemaObj = schemaSource.lookup(schema, true);
+    if (!schemaObj)
+        throw new Error('Schema ' + schema + ' could not be found for extension '
+                        + extension.metadata.uuid + '. Please check your installation.');
+
+    return new Gio.Settings({ settings_schema: schemaObj });
 }
-
-function removeInjection(object, injection, name) {
-	if (injection[name] === undefined)
-		delete object[name];
-	else
-		object[name] = injection[name];
-}
-
-let injections=[];
-
-//------------------------------------------
-
-const NewFolderDialog = new Lang.Class({
-    Name: 'NewFolderDialog',
-    Extends: ModalDialog.ModalDialog,
-
-    _init : function(id) {
-        this.parent();
-
-
-        let label = new St.Label({ text: _("Enter a name") });
-
-        this.contentLayout.add(label, { x_fill: false,
-                                        x_align: St.Align.START,
-                                        y_align: St.Align.START });
-
-        let entry = new St.Entry({can_focus: true });
-        ShellEntry.addContextMenu(entry);
-
-        entry.label_actor = label;
-
-		this._entryText = entry.clutter_text;
-		this.contentLayout.add(entry, { y_align: St.Align.START });
-		this.setInitialKeyFocus(this._entryText);
-
-        this.setButtons([{ action: Lang.bind(this, this.close),
-                           label: _("Cancel"),
-                           key: Clutter.Escape }]);
-
-        this._entryText.connect('key-press-event', Lang.bind(this, function(o, e) {
-            let symbol = e.get_key_symbol();
-            if (symbol == Clutter.Return || symbol == Clutter.KP_Enter) {
-                this.popModal();
-                this._create(o.get_text(),
-                          e.get_state() & Clutter.ModifierType.CONTROL_MASK, id);
-                if (!this._commandError ||
-                    !this.pushModal())
-                    this.close();
-
-                return Clutter.EVENT_STOP;
-            }
-            return Clutter.EVENT_PROPAGATE;
-        }));
-    },
-
-	_create : function(newName, osefka, id) {
-			let folderId = "";
-			
-			let tmp0 = newName.split(" ");
-			for(var i = 0; i < tmp0.length; i++) {
-				folderId += tmp0[i];
-			}
-			
-			for(var i = 0; i < _folderList.length; i++) {
-				if (_folderList[i] == folderId) {
-					log('this appfolder already exists');
-					return;
-				}
-			}
-
-			_folderList.push(folderId);
-			_foldersSchema.set_strv('folder-children', _folderList);
-			
-			let path = '/org/gnome/desktop/app-folders/folders/' + folderId + '/';
-			let tmp1 = new Gio.Settings({ schema_id: 'org.gnome.desktop.app-folders.folder', path: path });
-			tmp1.set_string('name', newName);
-			
-			addToFolder(id, folderId);
-	},
-	
-	open: function() {
-        this._entryText.set_text('');
-        this.parent();
-    },
-});
-
-//-------------------------------------------------
-
-function isInFolder (id, folder) {
-	let isIn = false;
-	let content_ = folder.get_strv('apps');
-	for(var j=0;j<content_.length;j++){
-		if(content_[j] == id) {
-			isIn = true;
-		}
-	}
-	return isIn;
-}	
-//-------------------------------------------------
-
-function addToFolder(id, folder) {
-	let path = '/org/gnome/desktop/app-folders/folders/' + folder + '/';
-	let tmp2 = new Gio.Settings({ schema_id: 'org.gnome.desktop.app-folders.folder', path: path });
-
-	let content = tmp2.get_strv('apps');
-	content.push(id);
-	tmp2.set_strv('apps', content);
-	reload();
-}
-
-//-------------------------------------------------
-
-function reload() {
-//	Main.overview.viewSelector.appDisplay._views[1].view._redisplay();
-}
-
-//-------------------------------------------------
-
-function setNbColumns(setting) {
-	let test = Main.overview.viewSelector.appDisplay._views;
-	for (let i = 0; i < test.length; i++) {
-		test[i].view._grid._colLimit = setting;
-	}
-
-	let _folderIcons = Main.overview.viewSelector.appDisplay._views[1].view.folderIcons;
-	_folderIcons.forEach(function(i){
-		i.view._grid._colLimit = setting;
-	});
-}
-
-//-------------------------------------------------
-
-function enable() {
-
-	let _settings = Convenience.getSettings('org.gnome.shell.extensions.appfolders-manager');
-//****************************************************************
-	setNbColumns( _settings.get_int('columns-max') );
-//****************************************************************
-	_foldersSchema = new Gio.Settings({ schema_id: 'org.gnome.desktop.app-folders' });
-	_folderList = _foldersSchema.get_strv('folder-children');
-//****************************************************************
-	
-	injections['_redisplay'] = injectToFunction(AppDisplay.AppIconMenu.prototype, '_redisplay',  function(){
-		this._appendSeparator();
-//------------------------------------------
-		let addto = new PopupMenu.PopupSubMenuMenuItem(_("Add to"));
-		
-		//------------------------------------------
-		let newAppFolder = new PopupMenu.PopupMenuItem('+ ' + _("New AppFolder"));
-		newAppFolder.connect('activate', Lang.bind(this, function() {
-			let id = this._source.app.get_id();
-			let dialog = new NewFolderDialog(id);
-			dialog.open();
-		}));
-		addto.menu.addMenuItem(newAppFolder);
-		//------------------------------------------
-		
-		for (var i = 0 ; i < _folderList.length ; i++) {
-			let _folder = _folderList[i];
-			
-			let _tmp = new Gio.Settings({	schema_id: 'org.gnome.desktop.app-folders.folder',
-											path: '/org/gnome/desktop/app-folders/folders/' + _folder + '/'
-										});
-									
-			let shouldShow = !isInFolder( this._source.app.get_id(), _tmp );
-			
-			let item = new PopupMenu.PopupMenuItem( AppDisplay._getFolderName( _tmp ) );
-			
-			if(shouldShow) {
-				item.connect('activate', Lang.bind(this, function() {
-					let id = this._source.app.get_id();
-				
-					addToFolder(id, _folder);
-				}));
-				addto.menu.addMenuItem(item);
-			}
-		}
-		
-		this.addMenuItem(addto);
-//------------------------------------------
-		let removeFrom = new PopupMenu.PopupSubMenuMenuItem(_("Remove from"));
-		let shouldShow2 = false;
-		for (var i = 0 ; i < _folderList.length ; i++) {
-			let _folder = _folderList[i];
-			
-			let id = this._source.app.get_id();
-			
-			let _tmp = new Gio.Settings({	schema_id: 'org.gnome.desktop.app-folders.folder',
-											path: '/org/gnome/desktop/app-folders/folders/' + _folder + '/'
-										});
-										
-			let item = new PopupMenu.PopupMenuItem( AppDisplay._getFolderName( _tmp ) );
-			
-			let shouldShow = isInFolder(id, _tmp);
-			
-			if(shouldShow) {
-				item.connect('activate', Lang.bind(this, function() {
-				
-					let tmp = new Gio.Settings({	schema_id: 'org.gnome.desktop.app-folders.folder',
-													path:  '/org/gnome/desktop/app-folders/folders/' + _folder + '/'
-												});
-				
-					let pastContent = tmp.get_strv('apps');
-					let presentContent = [];
-					for(i=0;i<pastContent.length;i++){
-						if(pastContent[i] != id) {
-							presentContent.push(pastContent[i]);
-						}
-					}
-					tmp.set_strv('apps', presentContent);
-					reload();
-				}));
-	 			removeFrom.menu.addMenuItem(item);
-	 			shouldShow2 = true;
-	 		}
-		}
-		if (shouldShow2) {
-			this.addMenuItem(removeFrom);
-		}
-//------------------------------------------
-		let delAppfolder = new PopupMenu.PopupSubMenuMenuItem(_("Delete AppFolder"));
-		for (var i = 0 ; i < _folderList.length ; i++) {
-			let _folder = _folderList[i];
-			
-			let _tmp = new Gio.Settings({	schema_id: 'org.gnome.desktop.app-folders.folder',
-											path: '/org/gnome/desktop/app-folders/folders/' + _folder + '/'
-										});
-										
-			let item = new PopupMenu.PopupMenuItem( AppDisplay._getFolderName( _tmp ) );
-			
-			item.connect('activate', Lang.bind(this, function() {
-				let tmp = [];
-				for(i=0;i<_folderList.length;i++){
-					if(_folderList[i] == _folder) {}
-					else {
-						tmp.push(_folderList[i]);
-					}
-				}
-				_foldersSchema.set_strv('folder-children', tmp);
-				
-				if ( _settings.get_boolean('total-deletion') ) {
-					let path = '/org/gnome/desktop/app-folders/folders/' + _folder + '/';
-					let tmp3 = new Gio.Settings({ schema_id: 'org.gnome.desktop.app-folders.folder', path: path });
-					tmp3.reset('apps');
-					tmp3.reset('name');
-				}				
-				
-				let timeoutId = Mainloop.timeout_add(200, Lang.bind(this, function() {
-					disable();
-					Mainloop.source_remove(timeoutId);
-					enable();
-				}));
-			}));
-        	delAppfolder.menu.addMenuItem(item);
-		}
-		this.addMenuItem(delAppfolder);
-		
-	//end of injections beyond the following line
-	});
-
-//end of enable()
-}
-
-//-------------------------------------------------
-
-function disable() {
-	removeInjection(AppDisplay.AppIconMenu.prototype, injections,  '_redisplay');
-	setNbColumns( 6 );
-}
+								  
